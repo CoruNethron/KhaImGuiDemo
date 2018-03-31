4d2dcd087ae97ecb5ef2036950e3eeb1b40ca272
Denis Golovkin <corunethron@gmail.com>
2018-03-31 13:49:56 +1000

    Replaced 4 spaces with tab in ImGuiDemo.hx

--- Sources/ImGuiDemo.hx
+++ Sources/ImGuiDemo.hx
@@ -25,7 +25,7 @@ import imgui.util.ImVec2;
 import imgui.draw.ImDrawData;
 
 class ImGuiDemo {
-    private static var pipeline:PipelineState;
+	private static var pipeline:PipelineState;
 	private static var framebuffer:Framebuffer;
 	private static var texunit:TextureUnit;
 	private static var imguiTexture:Image;
@@ -33,7 +33,7 @@ class ImGuiDemo {
 	private static var vtx:VertexBuffer;
 	private static var idx:IndexBuffer;
 
-    // --Mouse and keyboard input--
+	// --Mouse and keyboard input--
 	private static var inputX:Float;
 	private static var inputY:Float;
 	private static var inputDownL:Bool;
@@ -43,45 +43,45 @@ class ImGuiDemo {
 	private static var inputChars:String;
 
 	public function new() {
-        structure = new VertexStructure();
+		structure = new VertexStructure();
 		structure.add("pos", VertexData.Float3);
 		structure.add("col", VertexData.Float4);
 		structure.add("tex", VertexData.Float2);
 
 		inputChars = "";
 
-        isKeyDown = new Array();
+		isKeyDown = new Array();
 		for(i in 0...255) isKeyDown.push(false);
 
-        pipeline = new PipelineState();
-        pipeline.inputLayout = [structure];
-        pipeline.vertexShader = Shaders.imgui_vert;
-        pipeline.fragmentShader = Shaders.imgui_frag;
+		pipeline = new PipelineState();
+		pipeline.inputLayout = [structure];
+		pipeline.vertexShader = Shaders.imgui_vert;
+		pipeline.fragmentShader = Shaders.imgui_frag;
 
 		// --Use transparent blending mode, instead of opaque--
-        pipeline.blendSource = BlendingFactor.SourceAlpha;
-        pipeline.blendDestination = BlendingFactor.InverseSourceAlpha;
-        pipeline.compile();
+		pipeline.blendSource = BlendingFactor.SourceAlpha;
+		pipeline.blendDestination = BlendingFactor.InverseSourceAlpha;
+		pipeline.compile();
 
 		texunit = pipeline.getTextureUnit("texsampler");
 
-        var imguiIo = ImGui.getIO();
-        imguiIo.displaySize = ImVec2.create(1024, 768);
+		var imguiIo = ImGui.getIO();
+		imguiIo.displaySize = ImVec2.create(1024, 768);
 		imguiIo.renderDrawListsFn  = Callable.fromStaticFunction(onRender);
 		// --App crashes on attemt to copy if we don't register copy/paste callbacks--
 		kha.System.notifyOnCutCopyPaste(khaCut, khaCopy, khaPaste);
 
-        var atlas = Pointer.fromRaw(imguiIo.fonts).ref;
-        var width  : Int = 0;
-        var height : Int = 0;
-        var pixels : Array<Int> = null;
+		var atlas = Pointer.fromRaw(imguiIo.fonts).ref;
+		var width  : Int = 0;
+		var height : Int = 0;
+		var pixels : Array<Int> = null;
 
-        atlas.getTexDataAsRGBA32(pixels, width, height);
+		atlas.getTexDataAsRGBA32(pixels, width, height);
 
-        var buff:haxe.io.Bytes = haxe.io.Bytes.alloc(pixels.length);
+		var buff:haxe.io.Bytes = haxe.io.Bytes.alloc(pixels.length);
 		for(i in 0...pixels.length) buff.set(i, pixels[i]);
 
-        // --Seems like whole texture is white, but alpha channel changes--
+		// --Seems like whole texture is white, but alpha channel changes--
 		// --Multiplication of alpha channel moved to fragment shader--
 		imguiTexture = Image.fromBytes(buff, width, height, TextureFormat.RGBA32, Usage.StaticUsage);
 
@@ -89,36 +89,36 @@ class ImGuiDemo {
 		kha.input.Keyboard.get().notify(onKeyDown, onKeyUp, onKeyPress);
 
 		imguiIo.keyMap[ImGuiKey.Tab       ] = KeyCode.Tab;
-        imguiIo.keyMap[ImGuiKey.LeftArrow ] = KeyCode.Left;
-        imguiIo.keyMap[ImGuiKey.RightArrow] = KeyCode.Right;
-        imguiIo.keyMap[ImGuiKey.UpArrow   ] = KeyCode.Up;
-        imguiIo.keyMap[ImGuiKey.DownArrow ] = KeyCode.Down;
-        imguiIo.keyMap[ImGuiKey.PageUp    ] = KeyCode.PageUp;
-        imguiIo.keyMap[ImGuiKey.PageDown  ] = KeyCode.PageDown;
-        imguiIo.keyMap[ImGuiKey.Home      ] = KeyCode.Home;
-        imguiIo.keyMap[ImGuiKey.End       ] = KeyCode.End;
-        imguiIo.keyMap[ImGuiKey.Delete    ] = KeyCode.Delete;
-        imguiIo.keyMap[ImGuiKey.Backspace ] = KeyCode.Backspace;
-        imguiIo.keyMap[ImGuiKey.Enter     ] = KeyCode.Return;
-        imguiIo.keyMap[ImGuiKey.Escape    ] = KeyCode.Escape;
-        imguiIo.keyMap[ImGuiKey.A         ] = KeyCode.A;
-        imguiIo.keyMap[ImGuiKey.C         ] = KeyCode.C;
-        imguiIo.keyMap[ImGuiKey.V         ] = KeyCode.V;
-        imguiIo.keyMap[ImGuiKey.X         ] = KeyCode.X;
-        imguiIo.keyMap[ImGuiKey.Y         ] = KeyCode.Y;
-        imguiIo.keyMap[ImGuiKey.Z         ] = KeyCode.Z;
+		imguiIo.keyMap[ImGuiKey.LeftArrow ] = KeyCode.Left;
+		imguiIo.keyMap[ImGuiKey.RightArrow] = KeyCode.Right;
+		imguiIo.keyMap[ImGuiKey.UpArrow   ] = KeyCode.Up;
+		imguiIo.keyMap[ImGuiKey.DownArrow ] = KeyCode.Down;
+		imguiIo.keyMap[ImGuiKey.PageUp    ] = KeyCode.PageUp;
+		imguiIo.keyMap[ImGuiKey.PageDown  ] = KeyCode.PageDown;
+		imguiIo.keyMap[ImGuiKey.Home      ] = KeyCode.Home;
+		imguiIo.keyMap[ImGuiKey.End       ] = KeyCode.End;
+		imguiIo.keyMap[ImGuiKey.Delete    ] = KeyCode.Delete;
+		imguiIo.keyMap[ImGuiKey.Backspace ] = KeyCode.Backspace;
+		imguiIo.keyMap[ImGuiKey.Enter     ] = KeyCode.Return;
+		imguiIo.keyMap[ImGuiKey.Escape    ] = KeyCode.Escape;
+		imguiIo.keyMap[ImGuiKey.A         ] = KeyCode.A;
+		imguiIo.keyMap[ImGuiKey.C         ] = KeyCode.C;
+		imguiIo.keyMap[ImGuiKey.V         ] = KeyCode.V;
+		imguiIo.keyMap[ImGuiKey.X         ] = KeyCode.X;
+		imguiIo.keyMap[ImGuiKey.Y         ] = KeyCode.Y;
+		imguiIo.keyMap[ImGuiKey.Z         ] = KeyCode.Z;
 
-        Assets.loadEverything(function() {
+		Assets.loadEverything(function() {
 			System.notifyOnRender(render);
 		});
 	}
 
 	public function onKeyDown(code: kha.input.KeyCode) {
-        isKeyDown[code] = true;
+		isKeyDown[code] = true;
 	}
 
 	public function onKeyUp(code: kha.input.KeyCode) {
-        isKeyDown[code] = false;
+		isKeyDown[code] = false;
 	}
 
 	public function onKeyPress(char: String) {
@@ -158,7 +158,7 @@ class ImGuiDemo {
 		g.disableScissor();
 		g.end();
 
-        framebuffer = fb;
+		framebuffer = fb;
 
 		newFrame();
 		ImGui.showDemoWindow();
@@ -168,38 +168,38 @@ class ImGuiDemo {
 
 	private static function newFrame() {
 		var io = ImGui.getIO();
-        io.displaySize  = ImVec2.create(kha.System.windowWidth(), kha.System.windowHeight());
-        io.mousePos.x   = inputX;
-        io.mousePos.y   = inputY;
-        io.mouseDown[0] = inputDownL;
-        io.mouseDown[1] = inputDownR;
+		io.displaySize  = ImVec2.create(kha.System.windowWidth(), kha.System.windowHeight());
+		io.mousePos.x   = inputX;
+		io.mousePos.y   = inputY;
+		io.mouseDown[0] = inputDownL;
+		io.mouseDown[1] = inputDownR;
 		io.mouseWheel -= inputWheelDelta;
 		inputWheelDelta = 0;
 
-        io.keyCtrl      = isKeyDown[KeyCode.Control];
-        io.keyAlt       = isKeyDown[KeyCode.Alt];
-        io.keyShift     = isKeyDown[KeyCode.Shift];
-        io.keySuper     = isKeyDown[KeyCode.Win];
+		io.keyCtrl      = isKeyDown[KeyCode.Control];
+		io.keyAlt       = isKeyDown[KeyCode.Alt];
+		io.keyShift     = isKeyDown[KeyCode.Shift];
+		io.keySuper     = isKeyDown[KeyCode.Win];
 
-        io.keysDown[KeyCode.Tab      ] = isKeyDown[KeyCode.Tab];
-        io.keysDown[KeyCode.Left     ] = isKeyDown[KeyCode.Left];
-        io.keysDown[KeyCode.Right    ] = isKeyDown[KeyCode.Right];
-        io.keysDown[KeyCode.Up       ] = isKeyDown[KeyCode.Up];
-        io.keysDown[KeyCode.Down     ] = isKeyDown[KeyCode.Down];
-        io.keysDown[KeyCode.PageUp   ] = isKeyDown[KeyCode.PageUp];
-        io.keysDown[KeyCode.PageDown ] = isKeyDown[KeyCode.PageDown];
-        io.keysDown[KeyCode.Home     ] = isKeyDown[KeyCode.Home];
-        io.keysDown[KeyCode.End      ] = isKeyDown[KeyCode.End];
-        io.keysDown[KeyCode.Return   ] = isKeyDown[KeyCode.Return];
-        io.keysDown[KeyCode.Backspace] = isKeyDown[KeyCode.Backspace];
-        io.keysDown[KeyCode.Escape   ] = isKeyDown[KeyCode.Escape];
-        io.keysDown[KeyCode.Delete   ] = isKeyDown[KeyCode.Delete];
-        io.keysDown[KeyCode.A    ] = isKeyDown[KeyCode.A];
-        io.keysDown[KeyCode.C    ] = isKeyDown[KeyCode.C];
-        io.keysDown[KeyCode.V    ] = isKeyDown[KeyCode.V];
-        io.keysDown[KeyCode.X    ] = isKeyDown[KeyCode.X];
-        io.keysDown[KeyCode.Y    ] = isKeyDown[KeyCode.Y];
-        io.keysDown[KeyCode.Z    ] = isKeyDown[KeyCode.Z];
+		io.keysDown[KeyCode.Tab      ] = isKeyDown[KeyCode.Tab];
+		io.keysDown[KeyCode.Left     ] = isKeyDown[KeyCode.Left];
+		io.keysDown[KeyCode.Right    ] = isKeyDown[KeyCode.Right];
+		io.keysDown[KeyCode.Up       ] = isKeyDown[KeyCode.Up];
+		io.keysDown[KeyCode.Down     ] = isKeyDown[KeyCode.Down];
+		io.keysDown[KeyCode.PageUp   ] = isKeyDown[KeyCode.PageUp];
+		io.keysDown[KeyCode.PageDown ] = isKeyDown[KeyCode.PageDown];
+		io.keysDown[KeyCode.Home     ] = isKeyDown[KeyCode.Home];
+		io.keysDown[KeyCode.End      ] = isKeyDown[KeyCode.End];
+		io.keysDown[KeyCode.Return   ] = isKeyDown[KeyCode.Return];
+		io.keysDown[KeyCode.Backspace] = isKeyDown[KeyCode.Backspace];
+		io.keysDown[KeyCode.Escape   ] = isKeyDown[KeyCode.Escape];
+		io.keysDown[KeyCode.Delete   ] = isKeyDown[KeyCode.Delete];
+		io.keysDown[KeyCode.A    ] = isKeyDown[KeyCode.A];
+		io.keysDown[KeyCode.C    ] = isKeyDown[KeyCode.C];
+		io.keysDown[KeyCode.V    ] = isKeyDown[KeyCode.V];
+		io.keysDown[KeyCode.X    ] = isKeyDown[KeyCode.X];
+		io.keysDown[KeyCode.Y    ] = isKeyDown[KeyCode.Y];
+		io.keysDown[KeyCode.Z    ] = isKeyDown[KeyCode.Z];
 
 		io.addInputCharactersUTF8(inputChars);
 		inputChars = "";
@@ -208,73 +208,73 @@ class ImGuiDemo {
 	}
 
 	private static function setVertexColor(vBuf:Float32Array, col:Int, startIdx:Int) : Void {
-        vBuf.set(startIdx + 0, ((col >>  0) & 0xFF) / 255);
+		vBuf.set(startIdx + 0, ((col >>  0) & 0xFF) / 255);
 		vBuf.set(startIdx + 1, ((col >>  8) & 0xFF) / 255);
 		vBuf.set(startIdx + 2, ((col >> 16) & 0xFF) / 255);
 		vBuf.set(startIdx + 3, ((col >> 24) & 0xFF) / 255);
 	}
 
 	private static function onRender(_dataRawPtr : cpp.RawPointer<ImDrawData>) : Void {
-        // --There are probably some mem leaks in this function--
+		// --There are probably some mem leaks in this function--
 
-        var g = framebuffer.g4;
+		var g = framebuffer.g4;
 
-        var drawData = Pointer.fromRaw(_dataRawPtr).ref;
+		var drawData = Pointer.fromRaw(_dataRawPtr).ref;
 
 		var invHalfWW = 1.0 / (kha.System.windowWidth()  * .5);
 		var invHalfWH = 1.0 / (kha.System.windowHeight() * .5);
 
-        for(i in 0...drawData.cmdListsCount) {
-            var idxOffset = 0;
-            var cmdList = Pointer.fromRaw(drawData.cmdLists[i]).ref;
+		for(i in 0...drawData.cmdListsCount) {
+			var idxOffset = 0;
+			var cmdList = Pointer.fromRaw(drawData.cmdLists[i]).ref;
 			var cmdBuffer = cmdList.cmdBuffer.data;
 			var vtxBuffer = cmdList.vtxBuffer.data;
 			var idxBuffer = cmdList.idxBuffer.data;
 
 			for (j in 0...cmdList.cmdBuffer.size) {
-                var cmd  = cmdBuffer[j];
-                var it : Int = cast cmd.elemCount / 3;
+				var cmd  = cmdBuffer[j];
+				var it : Int = cast cmd.elemCount / 3;
 
 				vtx = new VertexBuffer(it*3, structure, Usage.StaticUsage);
 				idx = new IndexBuffer(it*3, Usage.StaticUsage);
 
-                var v = vtx.lock();
+				var v = vtx.lock();
 				var ii = idx.lock();
 			
-                for (tri in 0...it) {
-                    var baseIdx = idxOffset + (tri * 3);
-                    var idx1 = idxBuffer[baseIdx + 0];
-                    var idx2 = idxBuffer[baseIdx + 1];
-                    var idx3 = idxBuffer[baseIdx + 2];
-                    var vtx1 = vtxBuffer[idx1];
-                    var vtx2 = vtxBuffer[idx2];
-                    var vtx3 = vtxBuffer[idx3];
+				for (tri in 0...it) {
+					var baseIdx = idxOffset + (tri * 3);
+					var idx1 = idxBuffer[baseIdx + 0];
+					var idx2 = idxBuffer[baseIdx + 1];
+					var idx3 = idxBuffer[baseIdx + 2];
+					var vtx1 = vtxBuffer[idx1];
+					var vtx2 = vtxBuffer[idx2];
+					var vtx3 = vtxBuffer[idx3];
 					var tmul = tri * 27;
 
 					v.set(tmul+0, vtx1.pos.x * invHalfWW - 1); v.set(tmul+1, -(vtx1.pos.y * invHalfWH - 1.0)); v.set(tmul+2, 0.5); // Vertex coord
 					setVertexColor(v, vtx1.col, tmul +  3); // Vertex color
 					v.set(tmul+7, vtx1.uv.x); v.set(tmul+8, vtx1.uv.y); // Texture UV coord
-			        v.set(tmul+9, vtx2.pos.x * invHalfWW - 1); v.set(tmul+10, -(vtx2.pos.y * invHalfWH - 1.0)); v.set(tmul+11, 0.5);
-                    setVertexColor(v, vtx2.col, tmul + 12);
+					v.set(tmul+9, vtx2.pos.x * invHalfWW - 1); v.set(tmul+10, -(vtx2.pos.y * invHalfWH - 1.0)); v.set(tmul+11, 0.5);
+					setVertexColor(v, vtx2.col, tmul + 12);
 					v.set(tmul+16, vtx2.uv.x); v.set(tmul+17, vtx2.uv.y);
-			        v.set(tmul+18, vtx3.pos.x * invHalfWW - 1); v.set(tmul+19, -(vtx3.pos.y * invHalfWH - 1.0)); v.set(tmul+20, 0.5);
+					v.set(tmul+18, vtx3.pos.x * invHalfWW - 1); v.set(tmul+19, -(vtx3.pos.y * invHalfWH - 1.0)); v.set(tmul+20, 0.5);
 					setVertexColor(v, vtx3.col, tmul + 21);
 					v.set(tmul+25, vtx3.uv.x); v.set(tmul+26, vtx3.uv.y);
 
-                    ii[tri*3+0] = tri*3+0; ii[tri*3+1] = tri*3+1; ii[tri*3+2] = tri*3+2;
+					ii[tri*3+0] = tri*3+0; ii[tri*3+1] = tri*3+1; ii[tri*3+2] = tri*3+2;
 				}
 
 				vtx.unlock();
 				idx.unlock();
 
-                g.begin();
-		        g.setPipeline(pipeline);
+				g.begin();
+				g.setPipeline(pipeline);
 				g.setTexture(texunit, imguiTexture);
-		        g.setVertexBuffer(vtx);
-		        g.setIndexBuffer(idx);
+				g.setVertexBuffer(vtx);
+				g.setIndexBuffer(idx);
 				g.scissor(Std.int(cmd.clipRect.x), Std.int(cmd.clipRect.y), Std.int(cmd.clipRect.z - cmd.clipRect.x), Std.int(cmd.clipRect.w - cmd.clipRect.y));
-		        g.drawIndexedVertices();
-		        g.end();
+				g.drawIndexedVertices();
+				g.end();
 
 				vtx.delete();
 				idx.delete();
